917e22b7095d45edc8f4095fba958534792c4ef9
Felipe Rodrigues <150041905+feliperdgz7@users.noreply.github.com>
2024-03-05 19:37:56 -0300

    Modulo 26

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(7, 6));
+console.log(saudacao("Felipe"));
